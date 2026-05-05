9c4b13fdb89c55a44a1791b904ad9c49aeebbe8e
ilakkiyanit <ilakkiyanit@gmail.com>
2026-05-05 06:44:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
-from flask import Flask, request, send_file
+from flask import Flask, request
+import speech_recognition as sr
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "RAPO AUDIO DEBUG"
+    return "RAPO FREE AI READY"
 
 @app.route("/audio", methods=["POST"])
 def audio():
+
     data = request.data
+
     with open("voice.wav", "wb") as f:
         f.write(data)
-    return "saved"
 
-@app.route("/listen")
-def listen():
-    return send_file("voice.wav", mimetype="audio/wav")
+    r = sr.Recognizer()
+
+    try:
+        with sr.AudioFile("voice.wav") as source:
+
+            r.adjust_for_ambient_noise(source, duration=0.2)
+
+            audio_data = r.record(source)
+
+        text = r.recognize_google(audio_data, language="en-US")
+
+        print("You said:", text)
+
+        return text
+
+    except sr.UnknownValueError:
+        return "Could not understand"
+
+    except sr.RequestError:
+        return "Speech service error"
+
+    except Exception as e:
+        print(e)
+        return "Server error"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81)
